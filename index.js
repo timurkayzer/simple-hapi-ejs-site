d98dffbf1177ea0ae11af57e91ae7ff5de239e1b
Timur <timurkayzer@gmail.com>
2018-12-10 19:18:40 +0500

    added form to create posts

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const Hapi=require('hapi');
+const Hapi = require('hapi');
 const Vision = require('vision')
 const Page = require('./page-model')
 const Ejs = require('ejs')
 
 const server = Hapi.server({
-    host:'localhost',
-    port:8000
+    host: 'localhost',
+    port: 8000
 })
 
 const provision = async () => {
     await server.register(Vision);
 
     server.views({
-        engines: { ejs: Ejs },
+        engines: {ejs: Ejs},
         relativeTo: __dirname,
         path: 'templates/pages'
     })
@@ -21,34 +21,65 @@ const provision = async () => {
 
 // Add the route
 server.route({
-    method:'GET',
-    path:'/{slug}',
-    handler:async (request,h)=>{
+    method: 'GET',
+    path: '/{slug}',
+    handler: async (request, h) => {
         const slug = request.params.slug
         console.log(slug)
         const page = await Page.findOne({
-            where: {slug : slug}
+            where: {slug: slug}
         })
 
-        if(page)
-        return h.view('page', {
-            page: page
-        });
+        if (page)
+            return h.view('page', {
+                page: page
+            });
         else return '404'
 
     }
 })
 
 server.route({
-    method:'GET',
-    path:'/',
-    handler:async (request,h)=>{
+    method: 'GET',
+    path: '/',
+    handler: async (request, h) => {
         const pages = await Page.findAll()
 
-        if(pages)
+        if (pages)
             return h.view('index', {
                 pages: pages
             });
+    }
+})
+
+server.route({
+    method: 'GET',
+    path: '/create-blog',
+    handler: async (request, h) => {
+        return h.view('create');
+    }
+})
+
+server.route({
+    method: 'POST',
+    path: '/create-blog',
+    handler: async (request,h)=>{
+
+        const blog = Page.build({
+            title: request.payload.title,
+            slug: request.payload.slug,
+            full_text: request.payload.full_text,
+        })
+
+        try {
+            const res = await blog.save()
+            console.log(res)
+            return h.view('create')
+        }
+        catch (e) {
+            return e
+        }
+
     }
 })
 
@@ -57,14 +88,14 @@ async function start() {
 
     try {
         await provision()
-        await server.start();
+        await server.start()
     }
     catch (err) {
-        console.log(err);
-        process.exit(1);
+        console.log(err)
+        process.exit(1)
     }
 
-    console.log('Server running at:', server.info.uri);
+    console.log('Server running at:', server.info.uri)
 }
 
 process.on('unhandledRejection', (err) => {
